a7a43ad40ae83cea857c56bf7ac515f2448ef56c
Tyrael122 <kanbme@protonmail.com>
2023-08-19 01:03:55 -0300

    Adding unit tests

--- src/test/java/org/example/PercolationTest.java
+++ src/test/java/org/example/PercolationTest.java
@@ -5,7 +5,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PercolationTest {
+    private static final int gridSize = 5;
     @Test
-    void deve
+    void shouldPercolateWithOpenedStraightLine() {
+        Percolation percolation = new Percolation(gridSize);
+        for (int rowIndex = 0; rowIndex < gridSize; rowIndex++) {
+            percolation.open(rowIndex, 0);
+        }
 
+        assertEquals(gridSize, percolation.numberOfOpenSites());
+        assertTrue(percolation.percolates());
+    }
+
+    @Test
+    void shouldNotPercolateWithIncompleteStraightLine() {
+        Percolation percolation = new Percolation(gridSize);
+        for (int rowIndex = 0; rowIndex < gridSize - 1; rowIndex++) {
+            percolation.open(rowIndex, 0);
+        }
+
+        assertEquals(gridSize - 1, percolation.numberOfOpenSites());
+        assertFalse(percolation.percolates());
+    }
 }
